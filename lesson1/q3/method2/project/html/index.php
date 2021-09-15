57c3a139678ef0f33a889e24ce3cb7109bae6260
Yuriy Tigunov <192215@gmail.com>
2021-09-15 21:25:54 +0800

    Удалил лишнюю пустую строку в первом методе. Поправил расширение файла с .html на .phpво втором методе для того, чтобы нельзы было напрямую увидеть код из браузера

--- lesson1/q3/method2/project/html/index.php
+++ lesson1/q3/method2/project/html/index.php
@@ -4,4 +4,4 @@ $title = "Главная страница - страница обо мне";
 $h1 = "Информация обо мне";
 $year = "2018";
 
-include "site.html";
+include "site.php";
--- lesson1/q3/method3/project/html/index.php
+++ lesson1/q3/method3/project/html/index.php
@@ -3,7 +3,7 @@ $title = "Главная страница - страница обо мне";
 $h1 = "Информация обо мне";
 $year = "2018";
 
-$content = file_get_contents("templates/site.html");
+$content = file_get_contents("templates/site.php");
 
 $content = str_replace("{{ title }}", $title, $content);
 $content = str_replace("{{ h1 }}", $h1, $content);
