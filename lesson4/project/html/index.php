e75a3af0fd2d4c2927c54f4ac5e4edda05f561a2
Yuriy Tigunov <192215@gmail.com>
2021-09-24 19:31:02 +0800

    lesson4 task1

--- lesson4/project/html/index.php
+++ lesson4/project/html/index.php
@@ -1,9 +1,7 @@
 <?php
 $smallPhoto = scandir('./gallery_img/small');
 $smallPhoto = array_splice($smallPhoto, 2);
-
 ?>
-
 <!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
@@ -12,9 +10,9 @@ $smallPhoto = array_splice($smallPhoto, 2);
     <script type="text/javascript" src="./scripts/jquery-1.4.3.min.js"></script>
     <script type="text/javascript" src="./scripts/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
     <script type="text/javascript" src="./scripts/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-    <link rel="stylesheet" type="text/css" href="./scripts/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="./scripts/fancybox/jquery.fancybox-1.3.4.css" media="screen"/>
     <script type="text/javascript">
-        $(document).ready(function(){
+        $(document).ready(function () {
             $("a.photo").fancybox({
                 transitionIn: 'elastic',
                 transitionOut: 'elastic',
@@ -22,30 +20,32 @@ $smallPhoto = array_splice($smallPhoto, 2);
                 speedOut: 500,
                 hideOnOverlayClick: false,
                 titlePosition: 'over'
-            });	}); </script>
+            });
+        }); </script>
 
 </head>
 
 <body>
 <div id="main">
     <div class="post_title"><h2>Галерея 1</h2></div>
-<!--    <div class="gallery">-->
-<!--        <a rel="gallery" class="photo" href="gallery_img/big/01.jpg"><img src="gallery_img/small/01.jpg" width="150" height="100" /></a>-->
-<!--        <a rel="gallery" class="photo" href="gallery_img/big/02.jpg"><img src="gallery_img/small/02.jpg" width="150" height="100" /></a>-->
-<!--        <a rel="gallery" class="photo" href="gallery_img/big/03.jpg"><img src="gallery_img/small/03.jpg" width="150" height="100" /></a>-->
-<!--        <a rel="gallery" class="photo" href="gallery_img/big/04.jpg"><img src="gallery_img/small/04.jpg" width="150" height="100" /></a>-->
-<!--        <a rel="gallery" class="photo" href="gallery_img/big/05.jpg"><img src="gallery_img/small/05.jpg" width="150" height="100" /></a>-->
-<!--        <a rel="gallery" class="photo" href="gallery_img/big/06.jpg"><img src="gallery_img/small/06.jpg" width="150" height="100" /></a>-->
-<!--        <a rel="gallery" class="photo" href="gallery_img/big/07.jpg"><img src="gallery_img/small/07.jpg" width="150" height="100" /></a>-->
-<!--        <a rel="gallery" class="photo" href="gallery_img/big/08.jpg"><img src="gallery_img/small/08.jpg" width="150" height="100" /></a>-->
-<!--        <a rel="gallery" class="photo" href="gallery_img/big/09.jpg"><img src="gallery_img/small/09.jpg" width="150" height="100" /></a>-->
-<!--        <a rel="gallery" class="photo" href="gallery_img/big/10.jpg"><img src="gallery_img/small/10.jpg" width="150" height="100" /></a>-->
-<!--    </div>-->
+    <!--    <div class="gallery">-->
+    <!--        <a rel="gallery" class="photo" href="gallery_img/big/01.jpg"><img src="gallery_img/small/01.jpg" width="150" height="100" /></a>-->
+    <!--        <a rel="gallery" class="photo" href="gallery_img/big/02.jpg"><img src="gallery_img/small/02.jpg" width="150" height="100" /></a>-->
+    <!--        <a rel="gallery" class="photo" href="gallery_img/big/03.jpg"><img src="gallery_img/small/03.jpg" width="150" height="100" /></a>-->
+    <!--        <a rel="gallery" class="photo" href="gallery_img/big/04.jpg"><img src="gallery_img/small/04.jpg" width="150" height="100" /></a>-->
+    <!--        <a rel="gallery" class="photo" href="gallery_img/big/05.jpg"><img src="gallery_img/small/05.jpg" width="150" height="100" /></a>-->
+    <!--        <a rel="gallery" class="photo" href="gallery_img/big/06.jpg"><img src="gallery_img/small/06.jpg" width="150" height="100" /></a>-->
+    <!--        <a rel="gallery" class="photo" href="gallery_img/big/07.jpg"><img src="gallery_img/small/07.jpg" width="150" height="100" /></a>-->
+    <!--        <a rel="gallery" class="photo" href="gallery_img/big/08.jpg"><img src="gallery_img/small/08.jpg" width="150" height="100" /></a>-->
+    <!--        <a rel="gallery" class="photo" href="gallery_img/big/09.jpg"><img src="gallery_img/small/09.jpg" width="150" height="100" /></a>-->
+    <!--        <a rel="gallery" class="photo" href="gallery_img/big/10.jpg"><img src="gallery_img/small/10.jpg" width="150" height="100" /></a>-->
+    <!--    </div>-->
 
     <div class="gallery">
 
         <?php foreach ($smallPhoto as $fotoname): ?>
-            <a rel="gallery" class="photo" href="gallery_img/big/<?=$fotoname?>"><img src="gallery_img/small/<?=$fotoname?>" width="150" height="100" /></a>
+            <a rel="gallery" class="photo" href="gallery_img/big/<?= $fotoname ?>"><img
+                        src="gallery_img/small/<?= $fotoname ?>" width="150" height="100"/></a>
         <?php endforeach; ?>
 
     </div>
@@ -58,7 +58,8 @@ $smallPhoto = array_splice($smallPhoto, 2);
     <div class="gallery">
 
         <?php foreach ($smallPhoto as $fotoname): ?>
-            <a class="photo2" href="gallery_img/big/<?=$fotoname?>" target=_blank><img src="gallery_img/small/<?=$fotoname?>" width="150" height="100" /></a>
+            <a class="photo2" href="gallery_img/big/<?= $fotoname ?>" target=_blank><img
+                        src="gallery_img/small/<?= $fotoname ?>" width="150" height="100"/></a>
         <?php endforeach; ?>
 
     </div>
