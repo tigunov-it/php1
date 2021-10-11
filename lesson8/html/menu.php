d7be2ddb3f2032dca0c840ed2d2c424893f6dfa9
Yuriy Tigunov <192215@gmail.com>
2021-10-11 19:05:39 +0800

    lesson8

---
+++ lesson8/html/menu.php
@@ -0,0 +1,9 @@
+<div class="header">
+    <nav>
+        <a href="./index.php">Главная</a>
+        <a href="./calc.php?arg1=0&operation=add&arg2=0">Калькулятор</a>
+        <a href="./basket.php">Корзина</a>
+        <a href="./cabinet.php">Личный кабинет</a>
+        <a href="./cabinet.php?logout">Выход</a>
+    </nav>
+</div>
